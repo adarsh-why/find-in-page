39f6e1bb4ad85686dc97dcbaf0dd2ccfaf56676c
Adarsh Chandran <adarsh@storybrain.io>
2021-03-24 03:26:00 +0530

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,6 @@
-javascript:(function()%7Bfor(var b%3Ddocument.getElementsByTagName("p")%2Ca%3D0%3Ba<b.length%3Ba%2B%2B)b%5Ba%5D.style%5B"background-color"%5D%3D"%23C036F3"%7D)()%3Bvoid+0
+(function() {
+  var elts = document.getElementsByTagName('p');
+  for (var i = 0; i < elts.length; i++) {
+    elts[i].style['background-color'] = '#C036F3';
+  }
+})();
